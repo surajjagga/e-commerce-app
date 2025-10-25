2ca1105f5a047be4e4946adbd4be4d18bda96056
surajjagga <suraj.jagga@gmail.com>
2025-10-25 15:28:59 -0400

    Update commit

--- services/config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
+++ services/config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
@@ -9,7 +9,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+
+        System.out.println("Hello Feature");
+        SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
 }
